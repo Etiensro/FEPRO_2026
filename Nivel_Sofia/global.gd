184e8cc5bc6225afb651f163dc0a04fb8b0c99ab
Etiensro <hernandezordaze@gmail.com>
2026-09-02 23:16:07 -0600

    Revert "Resuelto conflicto en project.godot"
    This reverts commit 44557c8b7cf891a20bfef41cc904fea08296978d.

--- Nivel_Sofia/global.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-# Acumula los valores numéricos de las respuestas correctas
-# del nivel del carrito (V/F) y de los túneles (Opción múltiple)
-# Esta variable es la que definirá el acceso al video de la cinemática final.
-var suma_niveles = 0
-var intentos_restantes = 3  # O el valor inicial que utilices
